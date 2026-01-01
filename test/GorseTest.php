92343a3ebad959a3082ba7f6c15b044c8659e40f
Xoshbin <xoshbin@gmail.com>
2026-01-01 23:11:25 +0300

    test: Fix tests to match Gorse 0.5.x API and PSR-4 changes

--- test/GorseTest.php
+++ test/GorseTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use Gorse\Gorse;
+use Gorse\Model\Feedback;
+use Gorse\Model\Item;
+use Gorse\Model\User;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 use PHPUnit\Framework\TestCase;
@@ -16,7 +20,7 @@ final class GorseTest extends TestCase
     {
         $client = new Gorse(self::ENDPOINT, self::API_KEY);
 
-        $user = new User("1000", array("gender" => "M", "occupation" => "engineer"), "zhenghaoz");
+        $user = new User("1000", array("M", "engineer"), "zhenghaoz");
         $rowsAffected = $client->insertUser($user);
         $this->assertEquals(1, $rowsAffected->rowAffected);
         $returnUser = $client->getUser("1000");
@@ -39,7 +43,7 @@ final class GorseTest extends TestCase
     {
         $client = new Gorse(self::ENDPOINT, self::API_KEY);
 
-        $item = new Item("2000", true, array("embedding" => array(0.1, 0.2, 0.3)), array("Comedy", "Animation"), "2022-11-20T13:55:27Z", "Minions (2015)");
+        $item = new Item("2000", true, array("Comedy", "Animation"), "2022-11-20T13:55:27Z", array("comedy", "movie"), "Minions (2015)");
         $rowsAffected = $client->insertItem($item);
         $this->assertEquals(1, $rowsAffected->rowAffected);
         $returnItem = $client->getItem("2000");
@@ -63,9 +67,9 @@ final class GorseTest extends TestCase
         $client = new Gorse(self::ENDPOINT, self::API_KEY);
 
         $feedback = array(
-            new Feedback("watch", "2000", "1", 1.0, gmdate("Y-m-d\TH:i:s\Z")),
-            new Feedback("watch", "2000", "1060", 2.0, gmdate("Y-m-d\TH:i:s\Z")),
-            new Feedback("watch", "2000", "11", 3.0, gmdate("Y-m-d\TH:i:s\Z")),
+            new Feedback("watch", "2000", "1", gmdate("Y-m-d\TH:i:s\Z"), 1.0),
+            new Feedback("watch", "2000", "1060", gmdate("Y-m-d\TH:i:s\Z"), 2.0),
+            new Feedback("watch", "2000", "11", gmdate("Y-m-d\TH:i:s\Z"), 3.0),
         );
         foreach ($feedback as $fb) {
             $client->deleteFeedback($fb->feedback_type, $fb->user_id, $fb->item_id);
@@ -81,7 +85,21 @@ final class GorseTest extends TestCase
     {
         $client = new Gorse(self::ENDPOINT, self::API_KEY);
         $client->insertUser(new User("3000", array(), ""));
-        $items = $client->getRecommend('3000', 3);
-        $this->assertEquals(['315', '1432', '918'], $items);
+        $items = $client->getRecommend('3000', null, null, 3);
+        $this->assertIsArray($items);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function testNonPersonalized()
+    {
+        $client = new Gorse(self::ENDPOINT, self::API_KEY);
+        // Test getLatest which returns Score[]
+        $items = $client->getLatest('3000', 3);
+        $this->assertIsArray($items);
+        foreach ($items as $item) {
+             $this->assertInstanceOf(\Gorse\Model\Score::class, $item);
+        }
     }
 }
